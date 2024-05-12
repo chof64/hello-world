03a69fda3ea31112ef8fac3a12ace3420ba234eb
Chad Fernandez <developer@chadfernandez.me>
2024-05-13 01:28:04 +0800

    Update homepage with more information (#1)
    * feat: updated homepage with more information
    
    * Update pr-versioning.yml

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,13 @@ import React from "react";
 export default function Home() {
   return (
     <main className="flex h-screen items-center justify-center">
-      <div>Hello World</div>
+      <div>
+        <div className="text-center text-xl font-semibold">Hello World</div>
+        <p className="text-center">
+          This servers as a test repository for anything server or deployment
+          related tasks.
+        </p>
+      </div>
     </main>
   );
 }
